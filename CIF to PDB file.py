b210c5d395ef079f11ad224b36541a5a2ffb55ae
rdashcr7 <108886729+rdashcr7@users.noreply.github.com>
2025-05-04 21:20:09 -0400

    CIF to PDB file.py

--- CIF to PDB file.py
+++ CIF to PDB file.py
@@ -1,24 +1,22 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Wed Dec  4 14:08:07 2024
 
 @author: rickyanshumandash
 """
 
+#%% This python script is used to convert CIF files into pdb files which are easier to handle while using the openMM package.
+#%% You can use the AlphaFold server to generate CIF files of peptides or proteins which aren't available in PDB database.
+
 from Bio import PDB
-
-# Load the CIF file
-cif_file = 'fold_apoa_i_6cch_model_1.cif'  # Specify the path to your CIF file
+ 
+cif_file = 'input.cif'  
 parser = PDB.MMCIFParser()
-
-# Parse the CIF file
+ 
 structure = parser.get_structure('protein', cif_file)
-
-# Write to PDB file
-pdb_file = 'output_structure.pdb'  # Specify the desired output PDB file name
+ 
+pdb_file = 'output_structure.pdb' 
 io = PDB.PDBIO()
 io.set_structure(structure)
 io.save(pdb_file)
 
-print(f"PDB file saved as {pdb_file}")
+print(f"PDB file path: {pdb_file}")
