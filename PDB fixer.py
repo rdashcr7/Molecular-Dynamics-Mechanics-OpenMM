437b009d17b029429afcf84512ef526d359b28bb
rdashcr7 <108886729+rdashcr7@users.noreply.github.com>
2025-05-04 21:24:16 -0400

    PDB fixer.py

--- PDB fixer.py
+++ PDB fixer.py
@@ -1,28 +1,27 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue Dec 24 19:25:54 2024
 
 @author: RDASH
 """
 
+#%% This code can be used to fix pdb files where certain residue information, hydrogen atoms or other missing atoms might be missing. This is important before performing molecular dynamics simulations.
+
 from pdbfixer import PDBFixer
 from openmm.app import PDBFile
-
-# Load your PDB file
-fixer = PDBFixer(filename="3bmp.pdb")
+ 
+fixer = PDBFixer(filename="file.pdb")
 
 # Find missing residues and add missing atoms
 fixer.findMissingResidues()
 fixer.findMissingAtoms()
 fixer.addMissingAtoms()
-fixer.addMissingHydrogens(7.0)  # Add hydrogens at pH 7.0
+fixer.addMissingHydrogens(7.0)
 
-# Save the fixed PDB file
-with open("3bmp_fixed.pdb", "w") as output:
+with open("file_fixed.pdb", "w") as output:
     PDBFile.writeFile(fixer.topology, fixer.positions, output)
 
 
-with open('3bmp.pdb', 'r') as infile, open('cleaned_3bmp.pdb', 'w') as outfile:
+with open('file.pdb', 'r') as infile, open('cleaned_file.pdb', 'w') as outfile:
     for line in infile:
         if not line.startswith("HETATM") or "MPD" not in line:
             outfile.write(line)
